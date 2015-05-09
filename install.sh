13d12bc1b4cc8b1c42f0c04ac200d8aa924b1169
Devomat@LimeForce <deathpoison.dc@gmail.com>
2015-05-09 15:44:22 +0200

    added configs for root user ;)

--- install.sh
+++ install.sh
@@ -9,12 +9,17 @@ if [ "$(id -u)" != "0" ]; then
    cp bash_aliases ~/.bash_aliases
    cp shell_prompt.sh ~/.shell_prompt
 
+   sudo cp -R vim /root/.vim
+   sudo cp vimrc /root/.vimrc
+   sudo cp tmux.conf /root/.tmux.conf
    sudo cp bash_aliases /root/.bash_aliases
+   sudo cp shell_prompt.sh /root/.shell_prompt
 
-   cd # to home dir
    git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+   sudo git clone https://github.com/gmarik/Vundle.vim.git /root/.vim/bundle/Vundle.vim
 
    vim +PluginInstall +qall
+   sudo vim +PluginInstall +qall
    exit 1
 fi
 
