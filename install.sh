b55cf9165814e87f93acd5a76043ccf778de187c
Myself =) <deathpoison.dc@gmail.com>
2015-05-09 17:49:21 +0200

    added root support

--- install.sh
+++ install.sh
@@ -9,18 +9,20 @@ if [ "$(id -u)" != "0" ]; then
    cp bash_aliases ~/.bash_aliases
    cp shell_prompt.sh ~/.shell_prompt
 
-   sudo cp -R vim /root/.vim
-   sudo cp vimrc /root/.vimrc
-   sudo cp tmux.conf /root/.tmux.conf
-   sudo cp bash_aliases /root/.bash_aliases
-   sudo cp shell_prompt.sh /root/.shell_prompt
-
    git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-   sudo git clone https://github.com/gmarik/Vundle.vim.git /root/.vim/bundle/Vundle.vim
 
    vim +PluginInstall +qall
-   sudo vim +PluginInstall +qall
    exit 1
 fi
 
-echo 'dont run as sudo ;)'
+echo 'install for root user'
+
+cp -R vim /root/.vim
+cp vimrc /root/.vimrc
+cp tmux.conf /root/.tmux.conf
+cp bash_aliases /root/.bash_aliases
+cp shell_prompt.sh /root/.shell_prompt
+
+git clone https://github.com/gmarik/Vundle.vim.git /root/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
+
