27a6c04f9442c70b25164101702d4badc5b317cb
Devomat@LimeForce <deathpoison.dc@gmail.com>
2015-05-09 17:53:19 +0200

    added tmux power line

--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@ if [ "$(id -u)" != "0" ]; then
    cp tmux.conf ~/.tmux.conf
    cp bash_aliases ~/.bash_aliases
    cp shell_prompt.sh ~/.shell_prompt
+   cp tmux_prompt.sh ~/.tmux_prompt
 
    git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
@@ -22,6 +23,7 @@ cp vimrc /root/.vimrc
 cp tmux.conf /root/.tmux.conf
 cp bash_aliases /root/.bash_aliases
 cp shell_prompt.sh /root/.shell_prompt
+cp tmux_prompt.sh /root/.tmux_prompt
 
 git clone https://github.com/gmarik/Vundle.vim.git /root/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
