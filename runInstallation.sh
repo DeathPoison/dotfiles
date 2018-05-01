10c8bddba0397e1cef21b679baa72bc80e2943ba
DrEvil <evil@humanity.org>
2018-05-01 22:59:58 +0200

    added symlink for libraries to installer

--- runInstallation.sh
+++ runInstallation.sh
@@ -8,7 +8,12 @@
 ./installNim.sh
 source dotfiles/bash_aliases
 
+## TODO replace me with out libreary folder!
+ln -s ../../nim/libraries .
+
 nim c -r buildModules.nim &>/dev/null
 nim c installer.nim &>/dev/null
 
 sudo ./installer
+
+rm -rf libraries
